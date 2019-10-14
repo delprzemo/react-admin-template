9b20a0b2a8c609697385a851b4e7f3ff6a2a1344
Przemyslaw Strucinski <pst@kmdpoland.pl>
2019-10-14 15:00:11 +0200

    Init project

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'; 
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
+// f you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+// learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
