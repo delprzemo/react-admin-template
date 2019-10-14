398609fd6ba24c7513978290878d1b51d6b0de90
Przemyslaw Strucinski <pst@kmdpoland.pl>
2019-10-14 16:51:36 +0200

    Add css admin template

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'; 
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
